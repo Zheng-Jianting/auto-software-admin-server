a53ab5275c2e645ff8001ed2e016900dbee27847
zjt <374537202@qq.com>
2023-04-30 00:34:40 +0800

    service discovery & load balance & gateway

--- auto-software-service/src/main/java/com/zhengjianting/autosoftware/util/NetUtil.java
+++ auto-software-service/src/main/java/com/zhengjianting/autosoftware/util/NetUtil.java
@@ -1,5 +1,7 @@
 package com.zhengjianting.autosoftware.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -18,16 +20,16 @@ public class NetUtil {
             }
             if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
                 ipAddress = request.getRemoteAddr();
-                if (ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")) {
-                    // 根据网卡取本机配置的IP
-                    InetAddress inet = null;
-                    try {
-                        inet = InetAddress.getLocalHost();
-                    } catch (UnknownHostException e) {
-                        e.printStackTrace();
-                    }
-                    ipAddress = inet != null ? inet.getHostAddress() : ipAddress;
+            }
+            if (ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")) {
+                // 根据网卡取本机配置的IP
+                InetAddress inet = null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
                 }
+                ipAddress = inet != null ? inet.getHostAddress() : ipAddress;
             }
             // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
             if (ipAddress != null && ipAddress.length() > 15) { // "***.***.***.***".length()
@@ -40,6 +42,6 @@ public class NetUtil {
             ipAddress = "";
         }
 
-        return ipAddress;
+        return StringUtils.isEmpty(ipAddress) ? "127.0.0.1" : ipAddress;
     }
 }
