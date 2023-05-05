f78f0320464355c9eff8760a32c0471ce27e0bd4
zjt <374537202@qq.com>
2023-05-05 19:04:47 +0800

    create role

--- user-mgmt/src/main/java/com/zhengjianting/autosoftware/service/PermissionServiceI.java
+++ user-mgmt/src/main/java/com/zhengjianting/autosoftware/service/PermissionServiceI.java
@@ -5,7 +5,10 @@ import com.zhengjianting.autosoftware.entity.Permission;
 import com.zhengjianting.autosoftware.util.FlatPermissionTree;
 import com.zhengjianting.autosoftware.util.PermissionTree;
 
+import java.util.List;
+
 public interface PermissionServiceI extends IService<Permission> {
     PermissionTree getPermissionTree();
     FlatPermissionTree getFlatPermissionTree();
+    List<Long> getNonLeafPermissionIdList();
 }
